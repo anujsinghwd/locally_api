aedaaf60796c4e71aac5f06b7250e5fd1b2c8e51
anujsinghwd <anuj_singh@outlook.in>
2019-07-24 16:31:57 +0530

    doc update

--- docs/api_project.js
+++ docs/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.0.1",
   "description": "locally shop and products",
   "title": "Locally Api Docs",
-  "url": "http://51.15.196.66:5000/",
+  "url": "http://{base_url}/",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-07-24T10:45:24.738Z",
+    "time": "2019-07-24T11:01:53.857Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
