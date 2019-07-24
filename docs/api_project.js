ae7930b73e1b7799c08df4f4a13c6357789617fc
anujsinghwd <anuj_singh@outlook.in>
2019-07-24 16:10:11 +0530

    docs updated

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "locally_api",
+  "version": "0.0.1",
+  "description": "locally market apis",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-07-24T10:39:39.569Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
